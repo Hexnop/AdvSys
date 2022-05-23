a99573370098b2cd7deeeb679bf34fa827994cc5
hexnop <a_lord@hotmail.it>
2022-05-23 11:10:37 +0200

    Update 2

--- advsys.py
+++ advsys.py
@@ -6,12 +6,13 @@ from src.advsysc import AdvSysApt
 if __name__ == '__main__':
     parser = build_options()
     args = parser.parse_args()
+    advsys = AdvSysApt()
     if args.a:
         args.p = True
         args.sub = True
     if args.p:
         print("[+] Cleaning apt")
-        AdvSysApt().cache.clear()
+        advsys.cache.clear()
         print("[+] Checking: Packages")
         crp = CheckResidualPackage()
         if crp.check_residual_config():
@@ -19,8 +20,7 @@ if __name__ == '__main__':
             crp.do_autoremove()
     if args.sub:
         print("[+] Checking: Update")
-        asa = AdvSysApt()
-        if asa.do_update():
-            asa.do_upgrade()
+        if advsys.do_update():
+            advsys.do_upgrade()
     if True not in args.__dict__.values():
         parser.print_help()
