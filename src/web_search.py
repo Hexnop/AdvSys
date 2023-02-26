70ebf622e77f74699dcdd4ee75ed9ff52cc469f9
hexnop <a_lord@hotmail.it>
2023-02-26 15:30:30 +0100

    Update 2

--- src/web_search.py
+++ src/web_search.py
@@ -2,19 +2,30 @@ import urllib3
 import certifi
 import ssl
 import os
+import sys
 
 CA_CERTS = certifi.where()
 CERT_REQS = ssl.CERT_REQUIRED
 FILE_PATH = os.environ['HOME']
 
-pool = urllib3.PoolManager(cert_reqs=CERT_REQS, ca_certs=CA_CERTS)
-resp = pool.request("GET", "https://google.it/search?q=asd")
 
-print("Response Code:", resp.status, "\n")
+class Searcher:
+    def __init__(self):
+        self.pool = urllib3.PoolManager(cert_reqs=CERT_REQS, ca_certs=CA_CERTS)
 
-for k, v in resp.headers.items():
-    print(k, v)
+    def search(self, args):
+        resp = self.pool.request("GET", "https://google.it/search?q=" + args)
 
-f = open(os.path.join(FILE_PATH, "Desktop/google.html"), "wb")
-f.write(resp.data)
-f.close()
+        print("Response Code:", resp.status, "\n")
+
+        for k, v in resp.headers.items():
+            print(k, v)
+
+        print(resp.data)
+        f = open(os.path.join(FILE_PATH, "Desktop/google.html"), "wb")
+        f.write(resp.data)
+        f.close()
+
+
+if __name__ == '__main__':
+    Searcher().search(sys.argv[1])
